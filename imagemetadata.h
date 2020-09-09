00954d436d2d72702b70df11e876c8b6946de1db
Mirek Skrabal <mirekskrabal94@gmail.com>
2020-09-09 11:34:01 +0200

    add getter for path

--- imagemetadata.h
+++ imagemetadata.h
@@ -7,15 +7,18 @@
 class ImageMetaData : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QString name READ name)
+    Q_PROPERTY(QString name READ name NOTIFY nameChanged)
     QUrl m_path;
     QString m_name;
+signals:
+    void nameChanged();
 public:
     explicit ImageMetaData(QObject *parent = nullptr);
     ImageMetaData(QUrl &path) : m_path(path), m_name(path.fileName()){}
     ImageMetaData(QUrl &&path) : m_path(path), m_name(path.fileName()){}
 
     QString name() {return m_name;}
+    QUrl &url() {return m_path;}
 };
 
 #endif // IMAGEMETADATA_H
